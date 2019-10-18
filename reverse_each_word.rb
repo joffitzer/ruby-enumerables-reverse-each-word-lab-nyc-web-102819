1e9f365f0c347f8b5e5b85fdb2499f1f461770ce
Jonah Offitzer <joffitzer@gmail.com>
2019-10-18 18:01:27 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
 def reverse_each_word(string)
   string = string.split(" ")
   reversed_string = []
-  string.each do|i|
+  string.collect do|i|
     reversed_string.push(i.reverse)
   end
   reversed_string.join(" ")
